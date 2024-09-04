b4c0d82187cf99ba3077559ec6a97aa42ea523a1
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-09-04 14:03:21 +0200

    fixed in case the number was negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number > 5:
     print(f"Last digit of {number} is {number % 10} and is greater than 5")
 elif number < 6 and number != 0:
 
-    last = abs(number) % 10
+    last = abs(number) % -10
     print(f"Last digit of {number} is {last} and is less than 6 and not 0")
 elif number == 0:
     print(f"Last digit of {number} is 0 and is 0")
