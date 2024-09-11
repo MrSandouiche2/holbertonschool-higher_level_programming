c2a2b49f3e026a7dde3dbc41549fa40d03d0717e
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-09-11 16:57:28 +0200

    fixed 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for ligne in matrix:
         for i, element in enumerate(ligne):
             if i < len(ligne)-1:
-                print("{}".format(element), end=" ")
+                print("{:d}".format(element), end=" ")
             else:
-                print("{}".format(element), end="")
+                print("{:d}".format(element), end="")
         print()
