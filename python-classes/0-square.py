103f34666e71f39ada1b124164db43673f7701c8
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-09-18 01:51:58 +0200

    modified 0 again

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 class Square:
-    """test pour voir si ça fonctionne (juste un placeholder)
-    """
+    """test pour voir si ça fonctionne (juste un placeholder"""
     pass
+    """le truc pour pass"""
