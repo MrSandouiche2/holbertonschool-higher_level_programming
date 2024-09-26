9c8ff84ce37b9dd5a3c1dae685859d8ca20f6351
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-09-26 15:20:05 +0200

    fixed pycode

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""le programme"""
 
 
 def is_same_class(obj, a_class):
 
-    if type(obj) == a_class:
+    """placeholder"""
+
+    if type(obj) is a_class:
         return True
     else:
         return False
