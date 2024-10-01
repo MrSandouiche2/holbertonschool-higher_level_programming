d341a71811e0d83da3e7e89e57f29dc7275c85f3
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-10-01 11:15:26 +0200

    fixed 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,4 +4,4 @@ def read_file(filename=""):
     """Reads a text file and prints its content to stdout"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-    print(read_data)
+    print(read_data, end="")
