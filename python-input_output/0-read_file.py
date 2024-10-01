653b10f3add2b29f77d9d7769dc13d8fff6901e2
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-10-01 11:13:05 +0200

    fixed 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def read_file(filename="my_file_0.txt"):
-    """placeholder"""
+def read_file(filename=""):
+    """Reads a text file and prints its content to stdout"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)
