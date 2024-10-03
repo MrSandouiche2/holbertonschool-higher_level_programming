2b8bf7445b74737b224f73f18ee88bfd2aacf9a6
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-10-03 11:10:56 +0200

    fixed 5 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
+
 """le depart du programme"""
 
 import json
+
+
 def load_from_json_file(filename):
     """creer objet json"""
     with open(filename, 'r', encoding='utf-8') as f:
