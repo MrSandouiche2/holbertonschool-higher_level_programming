20d3ad164cf5248aa6cdda067b0e7d8c89cf0285
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-10-03 13:19:08 +0200

    fixed7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,16 +7,16 @@ import json
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-filename = "add_item.json"
+
 
 arg = sys.argv[1:]
 
+filename = "add_item.json"
 
-def name():
-    """placeholder"""
-    try:
-        items = load_from_json_file(filename)
-    except FileNotFoundError:
-        items = []
-    items.extend(arg)
-    save_to_json_file(items, filename)
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(arg)
+save_to_json_file(items, filename)
