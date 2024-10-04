0d6ca4793b6065ff271d9e700043a6edb2e7cef1
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-10-04 22:30:47 +0200

    pushed task

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""le depart"""
+
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """ initialisation"""
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file)
+    print(f"Data serialized and saved to '{filename}'.")
+
+def load_and_deserialize(filename):
+    """ initialisation"""
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
