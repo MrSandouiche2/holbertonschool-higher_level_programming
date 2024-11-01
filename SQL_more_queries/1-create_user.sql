08b23aa05e27062514d649d6e566e42b5627e521
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-11-01 16:42:26 +0100

    should fix 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,6 @@
 -- le truc pour la synthax
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
