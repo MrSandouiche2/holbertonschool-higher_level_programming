6b6090086cb5289caac495ef5107c54242c00a6a
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-11-01 16:44:16 +0100

    flushed the flushing

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
 -- le truc pour la synthax
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* to "user_0d_1"@"localhost";
