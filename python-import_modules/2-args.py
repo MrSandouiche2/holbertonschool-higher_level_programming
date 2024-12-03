f59dab1b70708c4af481189984c724710563935a
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-12-03 12:25:25 +0100

    pushed old

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-
 import sys
 
-if __name__ == "__main__":
 
-    if len(sys.argv) == 1:
-        print("{} arguments.".format(len(sys.argv)-1))
+def print_argv():
+    number = len(sys.argv) - 1
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(len(sys.argv)-1))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(number))
+
+    for count in range(1, len(sys.argv)):
+        print("{}: {}".format(count, sys.argv[count]))
+
+
+if __name__ == "__main__":
+    print_argv()
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+import sys
 
-from sys import argv
+
+result = 0
+for arg in range(1, len(sys.argv)):
+    number = int(sys.argv[arg])
+    result += number
+print(result)
 
 if __name__ == "__main__":
-
-    if len(argv) == 1:
-        print("{}".format(len(argv)-1))
-    else:
-        result = 0
-        for i in range(1, len(argv)):
-            result += int(argv[i])
-        print("{}".format(result))
+    pass
